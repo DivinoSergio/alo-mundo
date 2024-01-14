a77d1f89f0622827d32cae5e5af00d94e16c2c81
DivinoSergio <divinosergio.ms@gmail.com>
2024-01-14 20:40:48 -0300

    alterou webconfig

--- shelter-people/src/main/java/com/mendes/shelterpeople/controller/PersonController.java
+++ shelter-people/src/main/java/com/mendes/shelterpeople/controller/PersonController.java
@@ -10,6 +10,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,11 +21,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @RestController
 @RequestMapping("/person")
+@CrossOrigin(origins = "http://localhost:4200")
 public class PersonController {
 
      @Autowired
     private PersonService service;
-
+    
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<Person> findAll() {
         return service.findAll();
